b6a70e57e7025203cd341a34e6ab2b8446644542
winterTTr <winterTTr@51f57e5b-ef54-0410-a42a-5b17031ffd56>
2008-08-21 03:30:21 +0000

    Add win32export

---
+++ trunk/win32export/win32export.py
@@ -0,0 +1,52 @@
+"""
+-*- encoding=utf-8 -*-
+
+Author :  winterTTr
+Mail   :  winterTTr@gmail.com
+Desc   :  The 'export' command on win32 platform
+Module :  win32export.py
+Version:  $Reversion$
+NOTE   :  Make sure the pywin32 has been installed.
+
+"""
+
+try :
+    import win32gui
+    import win32con
+    import win32api
+except ImportError:
+    raise ImportError , "You Should install pywin32 if you want to use win32export"
+
+def export ( name , value , update_system = True ):
+    """
+    Export 'name' with 'value' to system .
+    You can let update_system=False , if you don't want to make the
+    change avaliable at once .( updateSystem is a time-consuming action.)
+    """
+    try :
+        modifyVariableInRegister( name , value )
+    except:
+        return False
+
+    if update_system :
+        updateSystem()
+
+    return True
+
+def modifyVariableInRegister( name , value ):
+    """
+    Add ( or modify ) the 'name' with 'value' to the register.
+    Register Postion : HKEY_CURRENT_USER/Environment
+    """
+    key = win32api.RegOpenKey( win32con.HKEY_CURRENT_USER,"Environment",0,win32con.KEY_ALL_ACCESS)
+    if not key : raise
+    win32api.RegSetValueEx( key , name , 0 , win32con.REG_SZ , value )
+    win32api.RegCloseKey( key )
+
+def updateSystem():
+    """
+    Call SendMessageTimeout to send broadcast to all window , notice
+    that the system setting is changed 
+    """
+    rc,dwReturnValue = win32gui.SendMessageTimeout( win32con.HWND_BROADCAST , win32con.WM_SETTINGCHANGE , 0 , "Environment" , win32con.SMTO_ABORTIFHUNG, 5000)
+
